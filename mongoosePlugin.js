184d32ffa75b7e275f717931ffcedaab3a7b8db3
Zeke Nierenberg <hampzan09@gmail.com>
2016-04-13 11:28:45 -0400

    exposing can mixin

--- mongoosePlugin.js
+++ mongoosePlugin.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const permissionSchema = require('./permissionSchema');
-
+const canMixin = require('./canMixin');
 /**
  * @description  This is a [mongoose plugin](http://mongoosejs.com/docs/plugins.html).
  *   It exports a function which modifies a mongoose schema.
@@ -12,48 +12,8 @@ const permissionSchema = require('./permissionSchema');
  *
  *   It is meant to be used on a sort of user model.
  * @module plugins/mongoose
- * @requires  roleManager
- * @requires lodash
+ * @requires  canMixin
  */
-
-const roleManager = require('./roleManager');
-const _ = require('lodash');
-
-/**
- * Checks if a user has the ability to perform an action on a noun
- * @memberOf plugins/mongoose
- * @param  {string} verb
- * @param  {string} noun
- * @return {boolean}
- */
-const can = function(verb, noun) {
-  if(_.filter(this.permissionsWhitelist, { verb, noun }).length) {
-    return true;
-  } else if(_.filter(this.permissionsBlacklist, { verb, noun }).length) {
-    return false;
-  } else {
-    return roleManager.can(this.roles, verb, noun);
-  }
-};
-
-/**
- * Determines if user has role or roles
- * @param  {String|Array}  r role
- * @return {Boolean}
- */
-const hasRole = function(r) {
-  if (typeof r === 'string'){
-    return this.roles.indexOf(r) !== -1;
-  } else if (Array.isArray(r)) {
-    let hasAllRoles = true;
-    r.forEach(function (role) {
-      if (this.roles.indexOf(role) === -1){
-        hasAllRoles = false;
-      }
-    }, this);
-    return hasAllRoles;
-  }
-};
 
 /**
  * @classDesc Mongoose plugin to provide roles to users
@@ -70,6 +30,5 @@ const mongoosePlugin = module.exports = function(schema) {
     permissionsBlacklist: [permissionSchema]
   });
 
-  schema.methods.can = can;
-  schema.methods.hasRole = hasRole;
+  Object.assign(schema.methods, canMixin);
 };
