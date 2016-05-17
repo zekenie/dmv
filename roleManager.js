af2805dc18be9f8df5767c8a6317efb4423e2dda
Zeke Nierenberg <hampzan09@gmail.com>
2016-05-17 11:38:30 -0500

    making it safe to work in react, build process iteration

--- roleManager.js
+++ roleManager.js
@@ -1,10 +1,19 @@
 'use strict';
 /**
  * @module  RoleManager
- * @extends {Map}
  */
 
-class RoleManager extends Map {
+
+/** @todo  extends Map */
+class RoleManager {
+
+  constructor() {
+    this.map = new Map();
+    this.set = this.map.set.bind(this.map);
+    this.get = this.map.get.bind(this.map);
+    this.has = this.map.has.bind(this.map);
+    this.values = this.map.values.bind(this.map);
+  }
   can(roles, verb, noun) {
     return roles.every(r => this.has(r)) && 
     roles.some(r => this.get(r).can(verb, noun));
