7d887e55a7c3e85b6bfeae0e330ee11aace68292
Andrew Ash <andrewash9@gmail.com>
2016-05-24 11:15:16 -0400

    exposed clear methods on nounManager and roleManager

--- nounManager.js
+++ nounManager.js
@@ -8,6 +8,7 @@ class NounManager {
     this.get = this.map.get.bind(this.map);
     this.has = this.map.has.bind(this.map);
     this.values = this.map.values.bind(this.map);
+    this.clear = this.map.clear.bind(this.map);
   }
 }
 
