b6c794bbc3c519d5ace363030cb124ea33c651b4
Thomas Kelly <tmkelly28@gmail.com>
2016-12-12 12:23:31 -0500

    allow getUser to return promise

--- angularPlugin.js
+++ angularPlugin.js
@@ -49,31 +49,35 @@ angular.module('dmv', [])
 
         $rootScope.$on('$stateChangeStart', function(event, next) {
           const user = getUser();
-          if(next && next.auth) {
-            if(!user) {
-              $rootScope.$broadcast('NOT_AUTHENTICATED');
-              event.preventDefault();
-              return;
-            }
-            if(next.auth === true) { return; }
-            if(typeof next.auth === 'function') {
-              if(!next.auth.call(event, user, next)) {
-                event.preventDefault();
-                $rootScope.$broadcast('NOT_AUTHORIZED');
-              }
-              return;
-            }
-            for(let verb in next.auth) {
-              if(next.auth.hasOwnProperty(verb)) {
-                let noun = next.auth[verb];
-                if(!user.can(verb, noun)) {
+          return Promise.resolve(user)
+            .then(user => {
+              if(next && next.auth) {
+                if(!user) {
+                  $rootScope.$broadcast('NOT_AUTHENTICATED');
                   event.preventDefault();
-                  $rootScope.$broadcast('NOT_AUTHORIZED');
                   return;
                 }
+                if(next.auth === true) { return; }
+                if(typeof next.auth === 'function') {
+                  if(!next.auth.call(event, user, next)) {
+                    event.preventDefault();
+                    $rootScope.$broadcast('NOT_AUTHORIZED');
+                  }
+                  return;
+                }
+                for(let verb in next.auth) {
+                  if(next.auth.hasOwnProperty(verb)) {
+                    let noun = next.auth[verb];
+                    if(!user.can(verb, noun)) {
+                      event.preventDefault();
+                      $rootScope.$broadcast('NOT_AUTHORIZED');
+                      return;
+                    }
+                  }
+                }
               }
-            }
-          }
+
+            });
         });
       }
     };
